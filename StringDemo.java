43bbd1fe7ea9e8239b08e709bf2e90251a80dd7b
Pratyaksh-00 <pratyakshsharma4450@gmail.com>
2025-09-05 09:36:23 +0530

    Add files via upload

---
+++ StringDemo.java
@@ -0,0 +1,27 @@
+public class StringDemo {
+    public static void main(String[] args) {
+        
+        // String name="Roshan";
+        // String name1="roshan";
+        // String name2=new String("Roshan");
+        // System.out.println(name==name1);
+        // System.out.println(name==name2);
+        
+        // String name="Roshan";
+        // String name2=new String("Roshan");//this has the add of heap
+        // //String name2=new String("Roshan").intern();//this has the add of SCP
+        // System.out.println(name==name2);
+        // System.out.println(name.equals(name2));
+
+        String name="Aayush";
+        String name2="212342";
+        System.out.println(name.equals(name2));
+        System.out.println(name.compareTo(name2));
+
+
+        String name4="Roshan Kumar";
+        for(int i=0;i<name4.length();i++){
+           
+         }
+    }
+}
